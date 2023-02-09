66d070383b37e1fcf71fb2c626e446d81e484e8f
toolboxaid <97194984+ToolboxAid@users.noreply.github.com>
2023-02-09 11:18:17 -0500

    Format document change

--- examples/FileSystem_test/FileSystem_test.ino
+++ examples/FileSystem_test/FileSystem_test.ino
@@ -1,11 +1,14 @@
 #include "Arduino.h"
 #include "TBA_FileSystem.h"
-
+TBA_FileSystem fileSystem = TBA_FileSystem();
 
 void setup()
 {
+    File dir = SD.open("/");
+    fileSystem.debugDirectoryList(dir, 0);
+    
 }
+
 void loop()
 {
 }
-
